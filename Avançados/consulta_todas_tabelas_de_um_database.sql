cbdf0eebbd1ed53432a978690c12a77813bca2e2
Silas Eduardo <90927889+Silas-ER@users.noreply.github.com>
2023-08-08 10:30:04 -0300

    Update consulta_todas_tabelas_de_um_database.sql

--- Avançados/consulta_todas_tabelas_de_um_database.sql
+++ Avançados/consulta_todas_tabelas_de_um_database.sql
@@ -1,8 +1,8 @@
--- Descoberta de nomes dos SCHEMAS --
+-- Descoberta de nomes dos SCHEMAS 
 SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA;
 
--- Descoberta de tabelas do SCHEMA -- 
+-- Descoberta de tabelas do SCHEMA 
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = 'nome_do_seu_database';
